695bd3395b54cb8f438456021a7f19dec49a2241
Calvin <cmrambach@gmail.com>
2022-08-03 11:52:18 -0400

    Add Leafy Grasses Textures

--- src/main/java/net/firebrandomega/autumn/world/feature/ModPlacedFeatures.java
+++ src/main/java/net/firebrandomega/autumn/world/feature/ModPlacedFeatures.java
@@ -14,16 +14,16 @@ public class ModPlacedFeatures {
     public static final RegistryEntry<PlacedFeature> LEAF_PILE = PlacedFeatures.register("leaf_pile",ModConfiguredFeatures.LEAF_PILE, CountPlacementModifier.of(2), SquarePlacementModifier.of(), PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of());
     public static final RegistryEntry<PlacedFeature> RED_AUTUMN_PLACED = PlacedFeatures.register(
             "red_autumn_placed", ModConfiguredFeatures.RED_AUTUMN_SPAWN, modifiers(
-                    PlacedFeatures.createCountExtraModifier(3, 0.1f, 2)));
+                    PlacedFeatures.createCountExtraModifier(4, 0.1f, 2)));
 
     public static final RegistryEntry<PlacedFeature> ORANGE_AUTUMN_PLACED = PlacedFeatures.register(
             "orange_autumn_placed", ModConfiguredFeatures.ORANGE_AUTUMN_SPAWN, modifiers(
-                    PlacedFeatures.createCountExtraModifier(3, 0.1f, 2)
+                    PlacedFeatures.createCountExtraModifier(4, 0.1f, 2)
             ));
 
     public static final RegistryEntry<PlacedFeature> YELLOW_AUTUMN_PLACED = PlacedFeatures.register(
             "yellow_autumn_placed", ModConfiguredFeatures.YELLOW_AUTUMN_SPAWN, modifiers(
-                    PlacedFeatures.createCountExtraModifier(3, 0.1f, 2)
+                    PlacedFeatures.createCountExtraModifier(4, 0.1f, 2)
             ));
 
 }

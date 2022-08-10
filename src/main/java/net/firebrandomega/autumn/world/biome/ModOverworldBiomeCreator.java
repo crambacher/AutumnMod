5549303e252a6f35d881c4cee60502189f04e51e
Calvin <cmrambach@gmail.com>
2022-08-10 11:58:10 -0400

    changed particle size

--- src/main/java/net/firebrandomega/autumn/world/biome/ModOverworldBiomeCreator.java
+++ src/main/java/net/firebrandomega/autumn/world/biome/ModOverworldBiomeCreator.java
@@ -9,6 +9,8 @@ import net.firebrandomega.autumn.world.feature.ModPlacedFeatures;
 import net.minecraft.client.sound.MusicType;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleType;
 import net.minecraft.sound.BiomeMoodSound;
 import net.minecraft.sound.MusicSound;
 import net.minecraft.sound.SoundEvents;
@@ -33,12 +35,12 @@ public class ModOverworldBiomeCreator extends OverworldBiomeCreator {
 
     public static final RegistryKey<Biome> AUTUMN_KEY = RegistryKey.of(Registry.BIOME_KEY, new Identifier("autumn", "autumn_biome"));
 
-    public static Biome createBiome(Biome.Precipitation precipitation, float temperature, float downfall, int waterColor, int waterFogColor, SpawnSettings.Builder spawnSettings, GenerationSettings.Builder generationSettings, @Nullable MusicSound music) {
-        return new Biome.Builder().precipitation(precipitation).temperature(temperature).downfall(downfall).effects(new BiomeEffects.Builder().waterColor(waterColor).waterFogColor(waterFogColor).fogColor(12638463).particleConfig(new BiomeParticleConfig(ModParticles.ORANGE_LEAF_PARTICLE,0.00625f)).particleConfig(new BiomeParticleConfig(ModParticles.RED_LEAF_PARTICLE,0.00625f)).particleConfig(new BiomeParticleConfig(ModParticles.YELLOW_LEAF_PARTICLE,0.00625f)).skyColor(OverworldBiomeCreator.getSkyColor(temperature)).moodSound(BiomeMoodSound.CAVE).music(music).build()).spawnSettings(spawnSettings.build()).generationSettings(generationSettings.build()).build();
+    public static Biome createBiome(Biome.Precipitation precipitation, float temperature, float downfall, int waterColor, int waterFogColor, SpawnSettings.Builder spawnSettings, GenerationSettings.Builder generationSettings, @Nullable MusicSound music, ParticleEffect particle) {
+        return new Biome.Builder().precipitation(precipitation).temperature(temperature).downfall(downfall).effects(new BiomeEffects.Builder().waterColor(waterColor).waterFogColor(waterFogColor).fogColor(12638463).particleConfig(new BiomeParticleConfig(particle,0.00625f)).particleConfig(new BiomeParticleConfig(ModParticles.RED_LEAF_PARTICLE,0.00625f)).particleConfig(new BiomeParticleConfig(ModParticles.YELLOW_LEAF_PARTICLE,0.00625f)).skyColor(OverworldBiomeCreator.getSkyColor(temperature)).moodSound(BiomeMoodSound.CAVE).music(music).build()).spawnSettings(spawnSettings.build()).generationSettings(generationSettings.build()).build();
     }
 
-    public static Biome createBiome(Biome.Precipitation precipitation, float temperature, float downfall, SpawnSettings.Builder spawnSettings, GenerationSettings.Builder generationSettings, @Nullable MusicSound music) {
-        return ModOverworldBiomeCreator.createBiome(precipitation, temperature, downfall, 4159204, 329011, spawnSettings, generationSettings, music);
+    public static Biome createBiome(Biome.Precipitation precipitation, float temperature, float downfall, SpawnSettings.Builder spawnSettings, GenerationSettings.Builder generationSettings, @Nullable MusicSound music, ParticleEffect particle) {
+        return ModOverworldBiomeCreator.createBiome(precipitation, temperature, downfall, 4159204, 329011, spawnSettings, generationSettings, music, particle);
     }
 
     private static void addParticles(GenerationSettings.Builder generationSettings){
@@ -77,6 +79,6 @@ public class ModOverworldBiomeCreator extends OverworldBiomeCreator {
         DefaultBiomeFeatures.addDefaultVegetation(builder2);
         DefaultBiomeFeatures.addSweetBerryBushes(builder2);
         MusicSound musicSound = MusicType.createIngameMusic(SoundEvents.MUSIC_OVERWORLD_JUNGLE_AND_FOREST);
-        return ModOverworldBiomeCreator.createBiome(Biome.Precipitation.RAIN,  0.7f, 0.8f, builder, builder2, musicSound);
+        return ModOverworldBiomeCreator.createBiome(Biome.Precipitation.RAIN,  0.7f, 0.8f, builder, builder2, musicSound, ModParticles.ORANGE_LEAF_PARTICLE);
     }
 }

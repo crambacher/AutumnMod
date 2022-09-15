7e5320146c29d85eebbe93e1649a44585d019a7e
Calvin <cmrambach@gmail.com>
2022-09-15 10:32:07 -0400

    Reverting Deer model

--- src/main/java/net/firebrandomega/autumn/entity/client/DeerRenderer.java
+++ src/main/java/net/firebrandomega/autumn/entity/client/DeerRenderer.java
@@ -2,7 +2,12 @@ package net.firebrandomega.autumn.entity.client;
 
 import net.firebrandomega.autumn.AutumnMod;
 import net.firebrandomega.autumn.entity.DeerEntity;
+import net.firebrandomega.autumn.entity.RaccoonEntity;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
@@ -20,6 +25,20 @@ public class DeerRenderer extends GeoEntityRenderer<DeerEntity> {
     @Override
     public Identifier getTextureResource(DeerEntity instance){
         return new Identifier(AutumnMod.MOD_ID,"textures/entities/deer/deer.png");
+    }
+
+    @Override
+    public RenderLayer getRenderType(DeerEntity animatable, float partialTicks, MatrixStack stack,
+                                     VertexConsumerProvider renderTypeBuffer, VertexConsumer vertexBuilder,
+                                     int packedLightIn, Identifier textureLocation) {
+        if(animatable.isBaby()) {
+            stack.scale(0.5f, 0.5f, 0.5f);
+        } else {
+            stack.scale(1f, 1f, 1f);
+        }
+
+        return super.getRenderType(animatable, partialTicks, stack, renderTypeBuffer, vertexBuilder,
+                packedLightIn, textureLocation);
     }
 
 }

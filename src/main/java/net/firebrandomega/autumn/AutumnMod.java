4453715192a670eb25eb8f8123e2d7f7ac6a8a0e
Calvin <cmrambach@gmail.com>
2022-09-22 15:55:39 -0400

    change autumn biome gen to JSON

--- src/main/java/net/firebrandomega/autumn/AutumnMod.java
+++ src/main/java/net/firebrandomega/autumn/AutumnMod.java
@@ -6,7 +6,6 @@ import net.firebrandomega.autumn.registry.ModBlocks;
 import net.firebrandomega.autumn.registry.ModEntities;
 import net.firebrandomega.autumn.registry.ModItems;
 import net.firebrandomega.autumn.registry.ModParticles;
-import net.firebrandomega.autumn.world.feature.ModConfiguredFeatures;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
@@ -24,7 +23,6 @@ public class AutumnMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		ModConfiguredFeatures.registerConfiguredFeatures();
 
 		ModBlocks.registerBlocks();
 		ModItems.registerItems();

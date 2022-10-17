4aaeaa11d9c05a9ebb96efecaeff9e0c410845a2
Calvin <cmrambach@gmail.com>
2022-10-16 22:13:40 -0400

    Adding owl to biome gen

--- src/main/java/net/firebrandomega/autumn/world/gen/ModEntitySpawn.java
+++ src/main/java/net/firebrandomega/autumn/world/gen/ModEntitySpawn.java
@@ -2,7 +2,6 @@ package net.firebrandomega.autumn.world.gen;
 
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.fabricmc.fabric.mixin.object.builder.SpawnRestrictionAccessor;
 import net.firebrandomega.autumn.registry.ModEntities;
 import net.firebrandomega.autumn.world.biome.ModOverworldBiomeCreator;
 import net.minecraft.entity.SpawnGroup;
@@ -22,11 +21,11 @@ public class ModEntitySpawn {
                 SpawnGroup.CREATURE, ModEntities.DEER,
                 50, 2, 5);
 
-        SpawnRestrictionAccessor.callRegister(ModEntities.RACCOON, SpawnRestriction.Location.ON_GROUND,
-                Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, AnimalEntity::isValidNaturalSpawn);
-        SpawnRestrictionAccessor.callRegister(ModEntities.SQUIRREL, SpawnRestriction.Location.ON_GROUND,
-                Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, AnimalEntity::isValidNaturalSpawn);
-        SpawnRestrictionAccessor.callRegister(ModEntities.DEER, SpawnRestriction.Location.ON_GROUND,
-                Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, AnimalEntity::isValidNaturalSpawn);
+//        SpawnRestrictionAccessor.callRegister(ModEntities.RACCOON, SpawnRestriction.Location.ON_GROUND,
+//                Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, AnimalEntity::isValidNaturalSpawn);
+//        SpawnRestrictionAccessor.callRegister(ModEntities.SQUIRREL, SpawnRestriction.Location.ON_GROUND,
+//                Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, AnimalEntity::isValidNaturalSpawn);
+//        SpawnRestrictionAccessor.callRegister(ModEntities.DEER, SpawnRestriction.Location.ON_GROUND,
+//                Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, AnimalEntity::isValidNaturalSpawn);
     }
 }
